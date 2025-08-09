9f7bd89956c13c5f026d4961c9c59b6c6889c463
Jazmine Chargualaf <71017555+CodeWithJazmine@users.noreply.github.com>
2025-08-09 11:37:31 -0700

    feat: refactor Navigation component to use CSS classes for styling; remove inline styles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,25 +5,15 @@ import { ROUTES } from "../constants/constants"
 export default function Navigation() {
     const { user } = useAuth();
 
-    {/*TODO: Move styling to .css!*/ }
-    const buttonStyle = {
-        display: 'inline-block',
-        padding: '0.5rem 1rem',
-        backgroundColor: '#4a5568',
-        color: 'white',
-        textDecoration: 'none',
-        borderRadius: '4px'
-    }
-
     return (
         <div>
             {!user ? (
                 <nav>
-                    <Link to={ROUTES.SIGNIN} style={buttonStyle}>Sign In</Link>
+                    <Link to={ROUTES.SIGNIN} className="button">Sign In</Link>
                 </nav>
             ) : null}
             {user ? (
-                <nav style={{ display: 'flex', gap: '1rem' }}>
+                <nav className="nav">
                     <Link to={ROUTES.HOME}>Home</Link> | <Link to={ROUTES.RECIPES}>Recipes</Link>
                 </nav>
             ) : null}
